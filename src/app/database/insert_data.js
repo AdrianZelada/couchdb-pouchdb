6575617f27d64a378bb0e347c96949868e69550b
Guillermo David <gdavid.ptorrez@gmail.com>
2019-10-21 10:26:08 -0400

    IMP: Add service get users couchDB

---
+++ src/app/database/insert_data.js
@@ -0,0 +1,9 @@
+const InsertVaccinations = " " +
+" INSERT INTO th_vaccination_history (beneficiary_id,vaccine_ct,dose1,dose2,dose3,dose4,dose5,dose6,dose7,restarted,reg_date,other) VALUES " +
+" ('2952e7c0-baef-11e9-953f-4d3af8f4179e',6,'{/''date/'': /''2019-10-09/'', /''state/'': /''1/''}','{/''date/'': /''2019-10-19/'', /''site/'': 2, /''state/'': /''1/''}','{/''date/'': /''2019-10-24/'', /''site/'': 2, /''state/'': /''2/''}',NULL,NULL,NULL,NULL,1,'2019-09-30 00:00:00',NULL)"+
+",('1d2797e0-e153-11e9-a751-45dd2d09bc78',8,NULL,NULL,NULL,NULL,NULL,NULL,NULL,1,'2019-10-03 11:36:25',NULL)"+
+",('1d2797e0-e153-11e9-a751-45dd2d09bc78',2,'{/''date/'': /''2019-10-19/'', /''state/'': /''2/''}',NULL,NULL,NULL,NULL,NULL,NULL,1,'2019-10-03 00:00:00',NULL)"+
+";";
+
+
+export { InsertVaccinations }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../services/users.service';
+import { Observable, from } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  users$: Observable<any>;
+  constructor(private userService: UsersService) {
 
-  constructor() {}
+  }
+
+  ngOnInit() {
+    console.log("this.userService.users$", this.userService.users$);
+    // this.users$ = this.userService.getUsers();
+  }
+
+  async getAll() {
+    console.log('getALL HOME PAGE');
+    const users = await this.userService.getUsers();
+    console.log('users', users);
+    this.users$ = users.rows;
+  }
 
 }
