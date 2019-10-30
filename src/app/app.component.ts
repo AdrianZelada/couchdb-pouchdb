dfa8d8bede71d773c15eaad4ee2b135eb1b80883
adrian zelada torrez <zelada_torrez@hotmail.com>
2019-10-30 00:25:41 -0400

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -48,7 +48,7 @@ export class AppComponent {
     //   console.log(data);
     // })
 
-    this.medical.getAll();
+    // this.medical.getAll();
     this.beneficiary.getAll();
 
     this.medical.appoitments$.subscribe((response) => {
